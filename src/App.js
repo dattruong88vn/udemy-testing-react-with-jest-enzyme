5d2715d97194838ab53a85ef454c3dd00c72d9bf
dattruong <dat.truong@propzy.com>
2022-03-11 11:10:23 +0700

    react + hook + rxjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FirstPerson, SecondPerson, PersonSwitcher } from './components';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <PersonSwitcher />
+    <Routes>
+      <Route path="/">
+        <Route index path="first-person" element={<FirstPerson />} />
+        <Route path="second-person" element={<SecondPerson />}  />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
