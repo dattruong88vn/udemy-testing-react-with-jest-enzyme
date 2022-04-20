d105721ae146d47ee61a8d9dec6e138b68e5d2c0
dattruong <dat.truong@propzy.com>
2022-04-20 14:50:52 +0700

    rm rxjs and init source for testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { FirstPerson, SecondPerson, PersonSwitcher } from './components';
 import './index.css';
 
-const App = () => (
-  <BrowserRouter>
-    <PersonSwitcher />
-    <Routes>
-      <Route path="/">
-        <Route index path="first-person" element={<FirstPerson />} />
-        <Route path="second-person" element={<SecondPerson />}  />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => <div>learn react</div>;
 
 export default App;
