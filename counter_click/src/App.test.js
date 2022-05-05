5752f4f055ae3f289e54280d2b01387916dcad5e
dattruong <dat.truong@propzy.com>
2022-05-05 17:09:23 +0700

    construtor code

---
+++ counter_click/src/App.test.js
@@ -0,0 +1,13 @@
+
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+import App from './App';
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
+})
+
+test('renders non-empty conponent without crashing', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.exists()).toBe(true);
+});
