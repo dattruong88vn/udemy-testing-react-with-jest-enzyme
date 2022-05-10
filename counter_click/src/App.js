35e9474e881554f01adca0d097e079209887a303
Dat Truong <dat.truong@propzy.com>
2022-05-10 20:45:43 +0700

    video 24

--- counter_click/src/App.js
+++ counter_click/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const App = () => <div data-test="component-app">
-
-</div>;
+const App = () => (
+    <div data-test="component-app">
+        <h1 data-test="counter-display">The counter is currently</h1>
+        <button data-test="increment-button">Increment</button>
+    </div>
+);
 
 export default App;
--- counter_click/src/App.test.js
+++ counter_click/src/App.test.js
@@ -5,16 +5,31 @@ import App from "./App";
 Enzyme.configure({
   adapter: new EnzymeAdapter(),
 });
+/**
+ * Factory function to create a ShallowWrapper for the App component.
+ * @function setup
+ * @returns {ShallowComponent}
+ * */
+const setup = () => shallow(<App />);
+const findByTestAttr = (wrapper, value) => wrapper.find(`[data-test='${value}']`);
 
 test("render without error", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 });
 
-test("render increment button", () => { });
+test("render increment button", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
+});
 
-test("renders counter display", () => { });
+test("renders counter display", () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.length).toBe(1);
+});
 
 test("counter display start at 0", () => { });
 
