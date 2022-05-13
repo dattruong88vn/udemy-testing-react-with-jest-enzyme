0ab15bc7c1d9847217f14f810762181e7c2e6a5a
Dat Truong <dat.truong@propzy.com>
2022-05-14 06:27:48 +0700

    video 54

---
+++ jotto/src/Input.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Input.propTypes = {
+
+};
+
+function Input(props) {
+    return (
+        <div data-test="component-input">
+
+        </div>
+    );
+}
+
+export default Input;
