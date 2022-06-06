ddc43e10537585dffd8f25191d57b0b3fd4caabc
Dat Truong <dat.truong@propzy.com>
2022-06-06 16:12:13 +0700

    video 65: app component render test

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,12 +1,14 @@
-import Congrats from "./Congrats"
-import GuessedWord from "./GuessedWord"
+import Congrats from "./Congrats";
+import GuessedWord from "./GuessedWord";
 
 const App = () => (
-    <div className="container ">
-        <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWord guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]} />
-    </div>
+  <div data-test="app-component" className="container ">
+    <h1>Jotto</h1>
+    <Congrats success={true} />
+    <GuessedWord
+      guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+    />
+  </div>
 );
 
 export default App;
--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,3 +1,14 @@
-import { shallow } from 'enzyme';
+import { shallow } from "enzyme";
 
-test('renders without error', () => { })
+import App from "./App";
+import { findByTestAttr } from "../test/testUtils";
+
+const setup = () => {
+  return shallow(<App />);
+};
+
+test("renders without error", () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "app-component");
+  expect(appComponent.length).toBe(1);
+});
